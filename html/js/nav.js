9c7679ab6f4bb83ec3ba1aa7d5a4211260cc50b2
deep-Turquoise <nicholasnelson10@gmail.com>
2017-06-21 16:26:45 -0700

    some more initial changes

---
+++ html/js/nav.js
@@ -0,0 +1,9 @@
+document.write('\
+    <div class="topnav">\
+        <h1>NICHOLAS NELSON</h1>\
+        <a>portfolio</a>\
+        <a>resume</a>\
+        <a>school</a>\
+        <a>photography</a>\
+    </div>\
+');
