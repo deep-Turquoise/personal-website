2b8be2d19a3b304b9a9f963547b55b9fb2d70f1c
deep-Turquoise <nicholasnelson10@gmail.com>
2017-06-27 10:00:07 -0700

    some tester code to use on server

--- html/js/nav.js
+++
@@ -1,9 +0,0 @@
-document.write('\
-    <div class="topnav">\
-        <h1>NICHOLAS NELSON</h1>\
-        <a>portfolio</a>\
-        <a>resume</a>\
-        <a>school</a>\
-        <a>photography</a>\
-    </div>\
-');
