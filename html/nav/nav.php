9c8b86eb226d2601d0f4cd29a33494bd68095016
deep-Turquoise <nicholasnelson10@gmail.com>
2017-06-27 12:54:06 -0700

    trying php

---
+++ html/nav/nav.php
@@ -0,0 +1,10 @@
+<?php
+print "<link rel=\"stylesheet\" href=\"nav/css/nav.css\">";
+print "<div class=\"nav_bar\">";
+    print "<div class=\"title\">nicholas nelson</div>";
+    print "<div class=\"block\">portfolio</div>";
+    print "<div class=\"block\">resume</div>";
+    print "<div class=\"block\">school</div>";
+    print "<div class=\"block\">photo blog</div>";
+print "</div>";
+?>
