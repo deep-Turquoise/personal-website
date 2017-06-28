3745f63f9ffdc5d2a7cbeebd79a3cddbe658bf58
deep-Turquoise <nicholasnelson10@gmail.com>
2017-06-27 17:31:05 -0700

    basic school set up

---
+++ html/pages/school/cst336/cst336.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>nick</title>
+        <link href='//fonts.googleapis.com/css?family=Oswald' rel='stylesheet'>
+        <link rel="stylesheet" href="pages/home/css/index.css">
+    </head>
+    <body>
+    <?php include 'nav/nav.php'; ?>
+    <h1>CST 336</h1>
+    </body>
+</html>
