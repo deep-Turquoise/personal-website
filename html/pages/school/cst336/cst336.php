078127ea38334c76e965c710d4dc83d842d35c56
deep-Turquoise <nicholasnelson10@gmail.com>
2017-06-27 18:06:38 -0700

    dot

--- html/pages/school/cst336/cst336.php
+++ html/pages/school/cst336/cst336.php
@@ -5,7 +5,7 @@
         <link rel="stylesheet" href="pages/home/css/index.css">
     </head>
     <body>
-    <?php include '//nav/nav.php'; ?>
+    <?php include '../../../../../../nav/nav.php'; ?>
     <h1>CST 336</h1>
     </body>
 </html>
