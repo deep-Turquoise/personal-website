80ad8a4c89fd21374062cbb99eb1f8deab596a4a
deep-Turquoise <nicholasnelson10@gmail.com>
2017-06-27 19:19:16 -0700

    adj

--- html/pages/school/cst336/cst336.php
+++ html/pages/school/cst336/cst336.php
@@ -8,19 +8,22 @@
     <body>
     <?php include '../../../nav/nav.php'; ?>
     <h1>CST 336</h1>
-    <div class="floatingTABLE">
-        <table style="width: 100%;">
-            <tr>
-                <th>Week 1</th>
-                <th>
-                    <ul>
-                        <li><a href="http://nicholasnelson.me/">Home Page</a></li>
-                        <li>Class Page - This Page</li>
-                    </ul>
-                </th>
-                <td><a href="http://nicholasnelson2142.blogspot.com/2017/06/cst336-internet-programming-module-1.html">Learning Journal</a></td>
-            </tr>
-        </table>
+    <div class="main_zone">
+        <img src="img/background.jpg">
+        <div class="floatingTABLE">
+            <table style="width: 100%;">
+                <tr>
+                    <th>Week 1</th>
+                    <th>
+                        <ul>
+                            <li><a href="http://nicholasnelson.me/">Home Page</a></li>
+                            <li>Class Page - This Page</li>
+                        </ul>
+                    </th>
+                    <td><a href="http://nicholasnelson2142.blogspot.com/2017/06/cst336-internet-programming-module-1.html">Learning Journal</a></td>
+                </tr>
+            </table>
+        </div>
     </div>
     </body>
 </html>
