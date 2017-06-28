1a7a434750048afa76d9d31c4401431e48830228
deep-Turquoise <nicholasnelson10@gmail.com>
2017-06-27 18:05:41 -0700

    dots

--- html/nav/nav.php
+++ html/nav/nav.php
@@ -1,5 +1,5 @@
 <?php
-print "<link rel=\"stylesheet\" href=\"nav/css/nav.css\">";
+print "<link rel=\"stylesheet\" href=\"../../../../../nav/css/nav.css\">";
 print "<div class=\"nav_bar\">";
     print "<div class=\"title\"><a href=\"index.php\">nicholas nelson</a></div>";
     print "<div class=\"block\"><a>portfolio</a></div>";
