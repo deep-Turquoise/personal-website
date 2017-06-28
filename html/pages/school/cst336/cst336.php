a0599277830a1b5a221b1d71e764b38d978d35e2
deep-Turquoise <nicholasnelson10@gmail.com>
2017-06-27 18:54:37 -0700

    basic class info

--- html/pages/school/cst336/cst336.php
+++ html/pages/school/cst336/cst336.php
@@ -6,7 +6,56 @@
         <link rel="stylesheet" href="css/cst336.css">
     </head>
     <body>
-    <?php include '../../../../../../nav/nav.php'; ?>
+    <?php include '../../../nav/nav.php'; ?>
     <h1>CST 336</h1>
+    <div class="floatingTABLE">
+        <table style="width:100%">
+            <tr>
+                <th>Week 1</th>
+                <th>
+                    <ul>
+                        <li><a href="nicholasnelson.me">Home Page</a></li>
+                        <li>Class Page - This Page</li>
+                    </ul>
+                </th>
+                <td><a href="http://nicholasnelson2142.blogspot.com/2017/06/cst336-internet-programming-module-1.html">Learning Journal</a></td>
+            </tr>
+            <tr>
+                <td>Week 2</td
+                <td>"coming soon.."</td>
+                <td>"Learning Journal"</td>
+            </tr>
+            <tr>
+                <td>Week 3</td>
+                <td>"coming soon.."</td>
+                <td>"Learning Journal"</td>
+            </tr>
+            <tr>
+                <td>Week 4</td>
+                <td>"coming soon.."</td>
+                <td>"Learning Journal"</td>
+            </tr>
+            <tr>
+                <td>Week 5</td>
+                <td>"coming soon.."</td>
+                <td>"Learning Journal"</td>
+            </tr>
+            <tr>
+                <td>Week 6</td>
+                <td>"coming soon.."</td>
+                <td>"Learning Journal"</td>
+            </tr>
+            <tr>
+                <td>Week 7</td>
+                <td>"coming soon.."</td>
+                <td>"Learning Journal"</td>
+            </tr>
+            <tr>
+                <td>Week 8</td>
+                <td>"coming soon.."</td>
+                <td>"Learning Journal"</td>
+            </tr>
+        </table>
+    </div>
     </body>
 </html>
