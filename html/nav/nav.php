807dbb3ca23d9c80155cf086066fdc40bb915290
deep-Turquoise <nicholasnelson10@gmail.com>
2017-06-27 19:05:00 -0700

    break down

--- html/nav/nav.php
+++ html/nav/nav.php
@@ -2,9 +2,9 @@
 print "<link rel=\"stylesheet\" href=\"../../../../../nav/css/nav.css\">";
 print "<div class=\"nav_bar\">";
     print "<div class=\"title\"><a href=\"http://nicholasnelson.me/\">nicholas nelson</a></div>";
-    print "<div class=\"block\"><a>portfolio</a></div>";
-    print "<div class=\"block\"><a>resume</a></div>";
+    //print "<div class=\"block\"><a>portfolio</a></div>";
+    //print "<div class=\"block\"><a>resume</a></div>";
     print "<a href=\"http://nicholasnelson.me/pages/school/cst336/cst336.php\"><div class=\"block\">school</div></a>";
-    print "<div class=\"block\"><a>photo blog</a></div>";
+    //print "<div class=\"block\"><a>photo blog</a></div>";
 print "</div>";
 ?>
