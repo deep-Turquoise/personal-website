323d767ea29ad9ce7f54ca3e90685db5818bfc48
deep-Turquoise <nicholasnelson10@gmail.com>
2017-06-27 17:45:10 -0700

    updated links

--- html/nav/nav.php
+++ html/nav/nav.php
@@ -1,10 +1,10 @@
 <?php
 print "<link rel=\"stylesheet\" href=\"nav/css/nav.css\">";
 print "<div class=\"nav_bar\">";
-    print "<div class=\"title\"><a>nicholas nelson</a></div>";
+    print "<div class=\"title\"><a href=\"index.php\">nicholas nelson</a></div>";
     print "<div class=\"block\"><a>portfolio</a></div>";
     print "<div class=\"block\"><a>resume</a></div>";
     print "<div class=\"block\"><a>school</a></div>";
-    print "<div class=\"block\"><a>photo blog</a></div>";
+    print "<div class=\"block\"><a href=\"pages\school\cst336\cst336.php\">photo blog</a></div>";
 print "</div>";
 ?>
