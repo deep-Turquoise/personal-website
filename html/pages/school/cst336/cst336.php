99883238897a43083c1682fc3acd14d01759ebae
deep-Turquoise <nicholasnelson10@gmail.com>
2017-06-27 18:56:46 -0700

    dot

--- html/pages/school/cst336/cst336.php
+++ html/pages/school/cst336/cst336.php
@@ -3,7 +3,7 @@
     <head>
         <title>nick</title>
         <link href='//fonts.googleapis.com/css?family=Oswald' rel='stylesheet'>
-        <link rel="stylesheet" href="css/cst336.css">
+        <link rel="stylesheet" href="./css/cst336.css">
     </head>
     <body>
     <?php include '../../../nav/nav.php'; ?>
