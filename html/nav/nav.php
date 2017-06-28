1e42728b167bdd3dae5a23046f766dec89b5292b
deep-Turquoise <nicholasnelson10@gmail.com>
2017-06-27 17:47:00 -0700

    fixes

--- html/nav/nav.php
+++ html/nav/nav.php
@@ -4,7 +4,7 @@ print "<div class=\"nav_bar\">";
     print "<div class=\"title\"><a href=\"index.php\">nicholas nelson</a></div>";
     print "<div class=\"block\"><a>portfolio</a></div>";
     print "<div class=\"block\"><a>resume</a></div>";
-    print "<div class=\"block\"><a>school</a></div>";
-    print "<div class=\"block\"><a href=\"pages\school\cst336\cst336.php\">photo blog</a></div>";
+    print "<a href=\"pages\school\cst336\cst336.php\"><div class=\"block\">school</div></a>";
+    print "<div class=\"block\"><a>photo blog</a></div>";
 print "</div>";
 ?>
