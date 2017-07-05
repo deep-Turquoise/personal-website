0dc2656136379f9cc82844b63c9c6c23edd56ea5
deep-Turquoise <nicholasnelson10@gmail.com>
2017-07-05 16:12:18 -0700

    fixes

--- html/pages/school/labs/lab2/PartB/partb.php
+++ html/pages/school/labs/lab2/PartB/partb.php
@@ -10,11 +10,12 @@
     <table border="1">
         <?php
             $num = rand(1, 99);
+            $org_num = $num;
             for($x = 0; $x < 10; ++$x) {
                 print "<tr>";
                 for($y = 0; $y < 10; ++$y) {
                     $img = "notch.png";
-                    if(num > 0) { $img = "circle.png"; $num -= 1; }
+                    if($num > 0) { $img = "circle.png"; $num -= 1; }
 
                     print "<td><img src=\"$img\"</td>";
                 }
@@ -24,7 +25,7 @@
         print "</div>";
 
         print "<div class=\"center_text\">";
-        print "<h1>The number is $num</h1>";
+        print "<h1>The number is $org_num</h1>";
         print "</div>";
         ?>
 </body>
